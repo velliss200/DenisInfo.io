1e2947a4935b1e14b1cd67d4821147cf36db28e8
velliss200 <velliss200@gmail.com>
2025-09-06 23:28:51 +0300

    Add files via upload

---
+++ 121.js
@@ -0,0 +1,25 @@
+const log = console.log;
+
+const mayo = document.getElementById("mayo");
+
+mayo.addEventListener("click", () => {
+  window.location.href = "122.html";
+});
+
+const milk = document.getElementById("milk");
+
+milk.addEventListener("click", () => {
+  window.location.href = "123.html";
+});
+
+const fat = document.getElementById("fat");
+
+fat.addEventListener("click", () => {
+  window.location.href = "124.html";
+});
+
+const linkBtn = document.getElementById("linkBtn");
+
+linkBtn.addEventListener("click", () => {
+  window.location.href = "main.html";
+});
